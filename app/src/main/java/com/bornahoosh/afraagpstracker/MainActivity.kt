de5a979aee676ce86552f36b0401910a0fc5f01c
Jalal Yadegar <jalaldgr@gmail.com>
2024-08-17 17:18:15 +0330

    add topBar

--- app/src/main/java/com/bornahoosh/afraagpstracker/MainActivity.kt
+++ app/src/main/java/com/bornahoosh/afraagpstracker/MainActivity.kt
@@ -6,8 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -23,7 +25,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AfraaGPSTrackerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(topBar = { AppBar() }, modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
@@ -48,4 +50,12 @@ fun GreetingPreview() {
     AfraaGPSTrackerTheme {
         Greeting("Android")
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppBar() {
+    TopAppBar(
+        title = { Text("Tracker Number") }
+    )
 }
